517d1e2c4247a46156a1f4048f725458e5ffd9c7
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-10-31 11:08:30 -0500

    This should be working.

--- results.php
+++ results.php
@@ -3,5 +3,4 @@ include("header.php");
 $id = $_GET['id'];
 $individualResults = individualResults($id);
 $completed = json_decode($individualResults[0]['tasksCompleted']);
-var_dump($completed);
 echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'completed'=>$completed));
