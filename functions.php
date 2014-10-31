6010a50de89299145a3c9ea4acb7e87917923901
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-10-31 09:49:58 -0500

    sql fix?

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 function returnTeamResults(){
     global $dbh;
-    $sql = "SELECT * FROM scoring";
+    //$sql = "SELECT * FROM scoring";
+    $sql = "SELECT * FROM `scoring` ORDER BY `score` ASC";
     $get = $dbh->prepare($sql);
     $get->execute();
     return $get->fetchAll();
