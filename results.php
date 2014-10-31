8484f043ba5379ee0b35bb2325c83b4e49d9b3c5
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-10-31 11:27:33 -0500

    Adding in more info to results page

--- results.php
+++ results.php
@@ -3,4 +3,9 @@ include("header.php");
 $id = $_GET['id'];
 $individualResults = individualResults($id);
 $completed = json_decode($individualResults[0]['tasksCompleted']);
-echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'completed'=>$completed));
+$rotations = $individualResults[0]['numberRotations'];
+$numberLoops = $individualResults[0]['numberLoops'];
+$penalties = $individualResults[0]['numberPenalties'];
+$team = $individualResults[0]['teamName'];
+echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'completed'=>$completed,
+                                                'rotations'=>$rotations,'loops'=>$numberLoops,'penalties'=>$penalties,'teamName'=>$team));
