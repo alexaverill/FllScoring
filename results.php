af38dd4e16c0d9e4ad503a379d44f700de8d7c7d
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-11-03 10:26:07 -0600

    Spelling mistake

--- results.php
+++ results.php
@@ -6,7 +6,7 @@ $completed = json_decode($individualResults[0]['tasksCompleted']);
 $rotations = $individualResults[0]['numberRotations'];
 $numberLoops = $individualResults[0]['numberLoops'];
 $penalties = $individualResults[0]['numberPenalties'];
-$round = $idividualResults[0]['round'];
+$round = $individualResults[0]['round'];
 $team = getTeamName($individualResults[0]['teamName']);
 $score = $individualResults[0]['score'];
 echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'score'=>$score,'round'=>$round,'completed'=>$completed,
