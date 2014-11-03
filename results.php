52f0e3c754729ed5a91642e4e4d99bd1edbcc4d7
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-11-03 10:25:01 -0600

    Adding in round display on results page

--- results.php
+++ results.php
@@ -6,7 +6,8 @@ $completed = json_decode($individualResults[0]['tasksCompleted']);
 $rotations = $individualResults[0]['numberRotations'];
 $numberLoops = $individualResults[0]['numberLoops'];
 $penalties = $individualResults[0]['numberPenalties'];
+$round = $idividualResults[0]['round'];
 $team = getTeamName($individualResults[0]['teamName']);
 $score = $individualResults[0]['score'];
-echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'score'=>$score,'completed'=>$completed,
+echo $twig->render('resultsTemplate.html',array('teamResults'=>$individualResults,'score'=>$score,'round'=>$round,'completed'=>$completed,
                                                 'rotations'=>$rotations,'loops'=>$numberLoops,'penalties'=>$penalties,'teamName'=>$team));
