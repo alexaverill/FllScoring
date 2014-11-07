2fcde7545e28fcd6d2c404199807b84929d73ee2
alex.averill2013@gmail.com <alex.averill2013@gmail.com>
2014-11-07 10:03:25 -0600

    Adding a skip if teams have two consecutive high scores

--- functions.php
+++ functions.php
@@ -14,9 +14,12 @@ function topFour(){
     $get = $dbh->prepare($sql);
     $get->execute();
     $teams = $get->fetchAll();
-    var_dump($teams);
+    //var_dump($teams);
     for($x =0; $x<count($teams);$x++){
-        
+        echo '<h3>'.$teams[$x]['teamName'].' '.$teams[$x]['score'].'</h3>';
+        if($teams[$x]['teamName']==$teams[$x+1]){
+            $x++
+        }
     }
 }
 function returnTeamCell(){
